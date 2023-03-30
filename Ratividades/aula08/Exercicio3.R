43efffcf878671acae9ef2e6573e82cc0e81a54d
hermeson <hermesondovalebeserra722@gmail.com>
2023-03-30 13:13:08 -0300

    DF

--- Ratividades/aula08/Exercicio3.R
+++ Ratividades/aula08/Exercicio3.R
@@ -1,8 +1,25 @@
+#Leia o dataset ibge_populacao.csv usando o R;
 ibge <- read.csv('https://raw.githubusercontent.com/natorjunior/db-atividades/main/ibge_populacao.csv')
+
+
+
 ibge
 
+
+
+# Separe apenas estado do Ceará;
+
+
+ceara <- ibge[ibge == 'CE',]
+
+
+
+# Filtre apenas o município de Fortaleza;
 #2304400 -> id de fortaleza
-sd(ibge$populacao)
 
-table(!complete.cases(ibge$populacao))
 
+
+ceara_fortaleza <- ceara[ceara$id_municipio == 2304400]
+
+
+ceara_fortaleza
--- Ratividades/aula08/exercicio2.R
+++ Ratividades/aula08/exercicio2.R
@@ -1,15 +1,22 @@
+#Leia o dataset casos_covid.csv usando o R;
 covid <- read.csv('https://raw.githubusercontent.com/natorjunior/db-atividades/main/casos_covid_ce.csv')
 covid
 
+
+
+# Calcule a média e desvio padrão dos casos (campo quantidade) da amostra;
 sd(covid$quantidade) # Desvio Padrão
+
+
+
 mean(covid$quantidade) #Média quantidade
-```
-```{r}
+
+
+
+# Faça um histograma usando o campo quantidade;
 hist(covid$quantidade)
-```
-```{r}
-boxplot(covid$mediaMovel)
-covid[covid$mediaMovel < 20,]
-```
-```{r}
+
+
+# Crie um boxplot para os campos quantidade e média móvel.
 boxplot(covid$quantidade)
+boxplot(covid$mediaMovel)
